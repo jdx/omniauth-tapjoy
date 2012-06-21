4bcd3f12c20dd5c06a2e842dd45a9833ed783477
dickeyxxx <jeff@dickey.xxx>
2012-06-20 20:25:29 -0700

    adding all values to info hash

--- lib/omniauth/strategies/tapjoy.rb
+++ lib/omniauth/strategies/tapjoy.rb
@@ -13,16 +13,7 @@ module OmniAuth
       uid { raw_info["id"] }
 
       info do
-        {
-          :id => raw_info["id"],
-          :email => raw_info["email"],
-          :first_name => raw_info["first_name"],
-          :last_name => raw_info["last_name"],
-          :name => "#{raw_info["first_name"]} #{raw_info["last_name"]}",
-          :created_at => raw_info["created_at"],
-          :facebook_id => raw_info["facebook_id"],
-          :is_employee => raw_info["is_employee"]
-        }
+        raw_info.to_hash
       end
 
       def raw_info
